77fe9aa95b60b458cea4243dc5c1e0f7428998ca
stephi1997 <stephiravi181097@gmail.com>
2022-11-15 18:47:47 +0530

    My Task

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("OMR");
+
+	}
+	private void greensAdayar() {
+System.out.println("Adayar");
+
+	}
+	private void greensTambaram() {
+		System.out.println("Tambaram");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("Velacherry");
+
+	}
+	private void greensAnnaNagar() {
+		System.out.println("AnnaNagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelacherry();
+		g.greensAnnaNagar();
+	}
+
+}
